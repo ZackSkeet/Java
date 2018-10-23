53df49e9b5ffad65ce3d010e7e328a24618cd470
ZackSkeet <43841725+ZackSkeet@users.noreply.github.com>
2018-10-23 10:37:21 +0800

    Add files via upload

---
+++ TemperatureConversion.java
@@ -0,0 +1,32 @@
+package HelloWorld1;
+
+// Temperature Conversion
+
+import java.util.Scanner;
+
+public class TemperatureConversion
+{
+public static void main( String [] args )
+{
+   // Constants 
+	float f, c;
+	f = 0;
+	c = 0;
+			
+	// Converting f to c
+	    Scanner scan = new Scanner (System.in);
+	    System.out.println("Please enter degrees F");
+	    f = scan.nextFloat();
+	    c = (5* (f - 32)) / 9f;
+	    System.out.println(f + " degrees F is " + c + " in degrees C");
+	  // Converting c to f
+	    Scanner scan1 = new Scanner (System.in);
+	    System.out.println("Please enter degrees C");
+	    c = scan1.nextFloat();
+	    f = (9 * c + 160) / 5f;
+	    System.out.println(c + " degrees C is " + f + " in degrees F");
+	    }	
+
+				
+}
+ 
